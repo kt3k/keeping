d4e0b887ea0b84b72ffcd4a5d317994ebd0201b4
Yoshiya Hinosawa <stibium121@gmail.com>
2014-03-08 13:35:49 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# keeping v0.0.1
+> A tool for measuring round-trip time of http requests under a Keep-Alived connection.
---
+++ bin/cli.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+
+/**
+ * keeping-cli v0.0.1
+ * A tool for measuring round-trip time of http requests under a Keep-Alived connection.
+ */
+
+var keeping = require('../lib/keeping');
+
+var usage = function () {
+    console.log('usage: keeping [url]');
+};
+
+var statistics = function (rtts) {
+    var avg = rtts.reduce(function (x, y) {
+        return x + y;
+    }) / rtts.length;
+
+    console.log('avg=' + avg + 'ms');
+};
+
+var cli = function () {
+
+    if (process.argv.length != 3) {
+        usage();
+        process.exit(1);
+    }
+
+    var urlStr = process.argv[2];
+
+    if (!/^https?:\/\//.test(urlStr)) {
+        usage();
+        process.exit(1);
+    }
+
+    keeping(urlStr, 10, console, statistics);
+};
+
+cli();
